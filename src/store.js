07601a328aa269eb1038c373079e32ee94f69574
Jonathan <jonas@github.com>
2026-04-26 11:48:29 -0400

    Tarea 5 completa

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
+// ya esta para la tarea 5
  const store = configureStore({
     reducer: {
         cart: cartReducer,
